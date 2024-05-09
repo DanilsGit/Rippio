09af94c446bc517155e96bed5b3cb0917cafbb97
DanilsGit <danielestebanmarquez12@gmail.com>
2024-05-09 12:46:35 -0500

    Add profile page with nav using React Router's NavLink, including sections for account settings, credit visibility, recent orders, and help center.

---
+++ Rippio/src/pages/Profile/ProfileHelp.jsx
@@ -0,0 +1,8 @@
+export function ProfileHelp(){
+    return (
+        <>
+            <h1>Profile Section</h1>
+            <p>Esta es la sección de ayuda</p>
+        </>
+    )
+}
---
+++ Rippio/src/pages/Profile/ProfileOrders.jsx
@@ -0,0 +1,38 @@
+import './profileOrders.css'
+import orders from '../../utilities/recentOrders.json'
+
+export function ProfileOrders() {
+    return (
+        <section className="ProfileOrders">
+            <header className='ProfileOrders-header'>
+                <h1>Últimos pedidos</h1>
+                <p>Aquí puedes consultar información de tu interés acerca de los pedidos que has hecho anteriormente.</p>
+            </header>
+            <section className='ProfileOrders-ordersContainer'>
+                {orders.map((order, index) => (
+                    <article key={index} className='ProfileOrders-orderItem'>
+                        <div className='ProfileOrders-orderItem-header'>
+                            <h2>{order.restaurante}</h2>
+                            <p>{order.fecha}</p>
+                            <p>ID {order.idPedido}</p>
+                        </div>
+                        <div className='ProfileOrders-orderItem-body'>
+                            <ul className='ProfileOrders-orderItem-body-ul'>
+                                {order.productos.map((product, index) => (
+                                    <li key={index} className='ProfileOrders-orderItem-body-li'>
+                                            <p className='ProfileOrders-orderItem-body-li-p'>{product.nombre}</p>
+                                            <p className='ProfileOrders-orderItem-body-li-p'>{product.precio}</p>
+                                    </li>
+                                ))}
+                            </ul>
+                            {
+                                order.observaciones === '' ? null : <p className='ProfileOrders-orderItem-body-observations'>Observaciones del pedido: <span>{order.observaciones}</span></p>
+                            }
+                            <p className='ProfileOrders-orderItem-body-p'>Costo total: <span>{order.CostoTotal}</span></p>
+                        </div>
+                    </article>
+                ))}
+            </section>
+        </section>
+    )
+}

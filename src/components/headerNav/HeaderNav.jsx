5599271d603c0b758b5435bddc6a2881e0195b00
Valti <valentinalondono911@gmail.com>
2024-05-25 12:17:36 -0500

    Update cart modal with new colors, sizes, and icon
    Modified the cart modal for an improved user experience:
    
    - Changed color scheme to enhance visual appeal.
    - Adjusted sizes of various elements for better readability and interaction.
    - Updated the cart icon to a more fitting design.

--- src/components/headerNav/HeaderNav.jsx
+++ src/components/headerNav/HeaderNav.jsx
@@ -37,9 +37,9 @@ export function HeaderNav() {
                             <button onClick={() => handleClickCartModal(toggleCartModal)} className='cartLink'>
                                 {
                                     cart.items.length > 0 ?
-                                        <img className='cartIcon' src='https://firebasestorage.googleapis.com/v0/b/rippio.appspot.com/o/icons%2FcartContainIcon.png?alt=media&token=a3667264-0a76-40ee-92cc-b2344651ab54' />
+                                        <img className='cartIcon' src='https://firebasestorage.googleapis.com/v0/b/rippio.appspot.com/o/icons%2FcartContainIcon.png?alt=media&token=acb40490-2b9a-4f8b-a335-25e1ced67b36' />
                                         :
-                                        <img className='cartIcon' src='https://firebasestorage.googleapis.com/v0/b/rippio.appspot.com/o/icons%2FcartIcon.png?alt=media&token=8544fcaa-130f-4eea-9122-47ada0a95082' />
+                                        <img className='cartIcon' src='https://firebasestorage.googleapis.com/v0/b/rippio.appspot.com/o/icons%2FcartIcon.png?alt=media&token=e8898b28-899f-4ef7-8468-513ffcde4f9e' />
                                 }
                             </button>
                         </div>

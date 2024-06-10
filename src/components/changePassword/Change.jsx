b1a19ced797c38054529dcd1aa1b18529687a0f3
Valti <valentinalondono911@gmail.com>
2024-06-09 19:53:32 -0500

    Add styles to change password components and page
    - Add styles to change and sendemail components
    - Add background and display to change password page
    - Do a function to switch components

--- src/components/changePassword/Change.jsx
+++ src/components/changePassword/Change.jsx
@@ -4,16 +4,17 @@ import './change.css'
 
 export function Change() {
     return (
-            <div className='change-background-container'>
-                <div className='change-container'>
-                    <h1 className='change-title'>Cambia tu contraseña</h1>
-                    <p className='change-text'>Ingresa tu nueva contraseña.</p>
-                    <form className='change-form'>
-                        <input type='password' placeholder='Nueva contraseña' className='change-input' />
-                        <input type='password' placeholder='Confirma tu nueva contraseña' className='change-input' />
-                        <button className='change-button'>Cambiar contraseña</button>
+            <div className='chg-background-container'>
+                <div className='chg-container'>
+                    <img src='https://firebasestorage.googleapis.com/v0/b/rippio.appspot.com/o/LoginPage%2FRippioHappy.png?alt=media&token=0dc72f88-d0da-446d-9a8e-70165ba3983c'></img>
+                    <h1 className='chg-title'>Cambia tu contraseña</h1>
+                    <p className='chg-text'>Ingresa tu nueva contraseña, recuerda elegir una contraseña segura para proteger tu información.</p>
+                    <form className='chg-form'>
+                        <input type='password' placeholder='Nueva contraseña' className='chg-input' />
+                        <input type='password' placeholder='Confirma tu nueva contraseña' className='chg-input' />
+                        <button className='chg-button'>Cambiar contraseña</button>
                     </form>
-                    <p className='change-text'>¿Recuerdas tu contraseña? <Link to='/login' className='change-link'>Inicia sesión</Link></p>
+                    <p className='chg-text'>¿Recuerdas tu contraseña? <Link to='/login' className='chg-link'>Inicia sesión</Link></p>
                 </div>
             </div>
     )

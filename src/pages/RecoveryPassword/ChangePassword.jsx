209050610b675d0c78e3dc50740f7fa31a07dc10
Valti <valentinalondono911@gmail.com>
2024-06-16 22:28:46 -0500

    Add reset password page
    * Create ResetPassword page
    * Add path in main
    * Add Change component in resetpassword

---
+++ src/pages/RecoveryPassword/ChangePassword.jsx
@@ -0,0 +1,13 @@
+
+import React, { useState } from 'react';
+import { SendEmail } from '../../components/changePassword/sendEmail.jsx';
+import './changePassword.css'
+
+export function ChangePassword() {
+
+    return (
+            <div className='change-background-container'>
+                <SendEmail />
+            </div>
+    )
+}
